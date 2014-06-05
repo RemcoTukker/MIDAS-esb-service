c1b18acf4ba33a3d2c948a88bd55e5e5a0c832b0
Remco Tukker <helium_2@hotmail.com>
2014-06-05 15:43:05 +0200

    readied for demo

--- src/main/java/com/almende/arum/midas/actions/HandleRequest.java
+++ src/main/java/com/almende/arum/midas/actions/HandleRequest.java
@@ -1,8 +1,11 @@
 package com.almende.arum.midas.actions;
 
+import java.io.IOException;
 import java.net.URI;
+import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import org.jboss.soa.esb.actions.annotation.Process;
 import org.jboss.soa.esb.lifecycle.annotation.Initialize;
@@ -19,6 +22,7 @@ import com.almende.eve.transport.Receiver;
 import com.almende.eve.transport.Transport;
 import com.almende.eve.transport.TransportFactory;
 import com.almende.eve.transport.http.HttpTransportConfig;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import cz.certicon.arum.core.messaging.ARUMMessage;
 import cz.certicon.arum.core.messaging.ARUMMessageFactory;
@@ -35,63 +39,117 @@ public class HandleRequest {
 
 	private static Logger logger = LoggerFactory.getLogger(HandleRequest.class);
 
+	private RpcTransform rpc = RpcTransformFactory.get(new SimpleHandler<Object>(this));
+	
 	private static final ScheduledExecutorService worker = 
 			  Executors.newSingleThreadScheduledExecutor();
 
 	private Transport transport;
 	//private RpcTransform rpc = RpcTransformFactory.get(new SimpleHandler<Object>(this));
+	private URI proxyAgentURI = URI.create("http://localhost:3000/agents/esbProxy");
 	
+	Random randomNumber = new Random();
 	
-	Runnable task = new Runnable() {
-	    public void run() {
-	    	/* Do something… */
-	    	///*	
-			// send one message to ourselves to see whether this works
-			try {
-				ARUMServiceInvoker serviceInvoker = 
-						ARUMServiceInvokerFactory.createARUMServiceInvoker("MIDAS", "MidasService");
-			
-				ARUMMessage message = ARUMMessageFactory.createMessage(ARUMMessageType.FIPA_REQUEST_REQUEST,
-					ARUMMessageLanguage.APPLICATION_XML, "testID11223");
-				message.setContent("duh");
-			
-				//message.setFrom(endpoint); //hrmmm what the hell is this now, why necessary?
-				//serviceInvoker.setDefaultFromHeader("MidasService", "MIDAS");
-				
-				//NOTE: This is not FIPA compliant!! FIPA only has async..
-				ARUMMessage response = serviceInvoker.deliverSync(message, 5000);
-				
-				//the following is FIPA compliant; I guess the answer then gets delivered to the .._reply queue
-					//TODO: check that out
-				//serviceInvoker.deliverAsync(message);
-			
-				logger.debug("got reply!" + response.getContent());
-				System.out.println("system reply " + response.getContent());
-				
-			} catch (ARUMMessageDeliveryException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (ARUMProtocolException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (ARUMUnexpectedMessageProcessingException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		//*/
-	    	
-	    	
-	    }
-	 };
+	protected void generateBusMessage(String content) {
+		try {
+			ARUMServiceInvoker serviceInvoker = 
+					ARUMServiceInvokerFactory.createARUMServiceInvoker("MIDAS", "MidasService");
+		
+			ARUMMessage message = ARUMMessageFactory.createMessage(ARUMMessageType.FIPA_REQUEST_REQUEST,
+				ARUMMessageLanguage.APPLICATION_XML, "testID11223");
+			message.setContent(content);
+			//message.setFrom(endpoint); //hrmmm what the hell is this now, why necessary?
+			//serviceInvoker.setDefaultFromHeader("MidasService", "MIDAS");
+			//NOTE: This is not FIPA compliant!! FIPA only has async..
+			ARUMMessage response = serviceInvoker.deliverSync(message, 5000);
+			//the following is FIPA compliant; I guess the answer then gets delivered to the .._reply queue
+				//TODO: check that out
+			//serviceInvoker.deliverAsync(message);
+			logger.debug("got reply!" + response.getContent());
+			System.out.println("system reply " + response.getContent());
+		
+		} 
+		catch (ARUMMessageDeliveryException e) { e.printStackTrace(); } 
+		catch (ARUMProtocolException e) {	e.printStackTrace(); } 
+		catch (ARUMUnexpectedMessageProcessingException e) { e.printStackTrace(); }
+		
+		return;
+	}
 	
+	 public class GenerateJob implements Runnable {
+		  private String workerName;
+		  private double time;
+		  public GenerateJob(String _workerName, double _time) {
+		    this.workerName = _workerName;
+		    this.time = _time; // in seconds
+		  }
+		  public void run() {
+			  // equivalent JS code: 
+			  // var message = {id:0, method:"routeEvent", params:{worker:worker, type: "jobStarted", timestamp: date.getTime() }};
+			  // eve.useServiceFunction("send", "local://esbProxy", message, function(answer){ console.log(answer); }); 
+			  
+			  generateBusMessage("{\"worker\":\"" + workerName + "\",\"type\":\"jobStarted\", \"timestamp\":" + String.valueOf(System.currentTimeMillis()) + "}");
+			  
+			  // now decide how long the job will take
+			  // eq. JS code:
+			  //var realTime = randgen.rnorm(jobTimes[worker ], 10);
+			  //if (realTime < 2) realTime = 2; // making sure we dont have negative or ultrashort times
+				
+			  double duration = time + randomNumber.nextGaussian() * 10;
+			  if (duration < 2) duration = 2;
+			  
+			  // finally, make sure that at that time the job finished stuff is run
+			  FinishJob fin = fR;
+			  if (workerName.equals("Ludo")) fin = fL;
+			  if (workerName.equals("Giovanni")) fin = fG;
+			  
+			  worker.schedule(fin, (long) duration, TimeUnit.SECONDS);
+		  }
+	}
+	 
+	GenerateJob gR = new GenerateJob("Remco", 40);
+	GenerateJob gG = new GenerateJob("Giovanni", 30);
+	GenerateJob gL = new GenerateJob("Ludo", 30);
+	
+	 public class FinishJob implements Runnable {
+		  private String workerName;
+		  private double time;
+		  public FinishJob(String _workerName) {
+		    this.workerName = _workerName;
+		  }
+		  public void run() {
+			  // equivalent JS code: 
+			  // var message = {id:0, method:"routeEvent", params:{worker:worker, type: "jobStarted", timestamp: date.getTime() }};
+			  // eve.useServiceFunction("send", "local://esbProxy", message, function(answer){ console.log(answer); }); 
+			  
+			  generateBusMessage("{\"worker\":\"" + workerName + "\",\"type\":\"jobFinished\", \"timestamp\":" + String.valueOf(System.currentTimeMillis()) + "}");
+			  
+			  // finally, make sure that at that time the job finished stuff is run
+			  GenerateJob gen = gR;
+			  if (workerName.equals("Ludo")) gen = gL;
+			  if (workerName.equals("Giovanni")) gen = gG;
+			  
+			  worker.schedule(gen, 2, TimeUnit.SECONDS);
+		  }
+	}
+	
+	FinishJob fR = new FinishJob("Remco");
+	FinishJob fG = new FinishJob("Giovanni");
+	FinishJob fL = new FinishJob("Ludo");
+	 
 	@Initialize
 	public void checkConfig() {
 		
 		logger.debug("initializing!");
 		//System.out.println("system initializing");
 	
+		//generate test incoming message
 		//worker.schedule(task, 5, TimeUnit.SECONDS);
-	
+		worker.schedule(gG, 5, TimeUnit.SECONDS);
+		worker.schedule(gR, 6, TimeUnit.SECONDS);
+		worker.schedule(gL, 7, TimeUnit.SECONDS);
+		
+		
 		final HttpTransportConfig config = new HttpTransportConfig();
 		config.setServletUrl("http://localhost:8080/MIDASGateway/agents/");
 		config.setId("gatewayAgent");
@@ -108,24 +166,46 @@ public class HandleRequest {
 				// check if we have a reply or a request; 
 				// reply either goes to
 				
-				// TODO; use the stuff for this that is already available from Eve
+				// TODO; this is never called because when doing a POST request we run into the error: 
+				//   The server encountered an internal error (Couldn't load transport) that prevented it from fulfilling this request.
+				// which is generated in EveServlet.java / DebugServlet.java for some reason
+				
+				// answers on requests that were send out are coming through though!
 				
 				if (incoming instanceof JSONRequest) {
 					
 					// either generate event
 					
-					// or forward request to ARUM service and wait for reply
+					
+					// or forward request to ARUM service
+
+					
+					// and send a synchronous answer
+					
+					JSONResponse response = new JSONResponse();					
+					response.setId(incoming.getId());
+					String result = "success";
+					response.setResult(result);
+										
+					try {
+						//transport.send(senderUrl, response.toString(), tag );
+						transport.send(proxyAgentURI, response.toString(), tag );
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}   
 					
 				} else if (incoming instanceof JSONResponse) {
 					// we can either keep track of reponses ourselves, or let Eve take care of that
-					// note that we may have to keep track of responses ourselves anyway, for the ARUN-side of affairs
+					// note that we may have to keep track of responses ourselves anyway, for the ARUM-side of affairs
 					
+					// we dont need to do anything with the response for now 
 					
 				}
 				
 				
 				
-				logger.info("Hi there:"+msg +" from:"+ senderUrl);
+				logger.info("Received msg:"+msg +" from:"+ senderUrl);
 			}
 		}));
 		//reqSocket.connect("tcp://localhost:5556");
@@ -156,12 +236,23 @@ public class HandleRequest {
 				
 		//do something with the message
 
-		
-		
 		// forward it over http 
 		
-		// do we even have a blocking wait for response?
+		JSONRequest request = new JSONRequest();					
+		//request.setId(0); //ID can stay null
+		request.setMethod("incomingEvent");
+		request.putParam("Content", message.getContent());
 		
+		System.out.println(request.toString());
+		
+		try {
+			transport.send(proxyAgentURI, request.toString(), null );
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}   
+		
+				
 		// send the answer as soon as we got it..
 	
 		ARUMMessage arumResponse = null;
